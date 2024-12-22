72e4e972d622ccaf9f8c966f6540ca550b8c9830
joaquin-sanchez-tonani <joaquinsancheztonani15@gmail.com>
2024-12-22 13:13:58 -0300

    change port

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 import mysql.connector
 
 def get_db_connection():
-    return mysql.connector.connect(
-        host="autorack.proxy.rlwy.net",
-        user="root",
-        password="gUwUGiXhDyajFXyMPEVyOduaMMeMlAYg",
-        database="railway"
-    )
-
-
-
+    try:
+        return mysql.connector.connect(
+            host="autorack.proxy.rlwy.net",  
+            user="root",                   
+            password="gUwUGiXhDyajFXyMPEVyOduaMMeMlAYg",  
+            database="railway",            
+            port=47280                      
+        )
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return None
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+autorack.proxy.rlwy.net
