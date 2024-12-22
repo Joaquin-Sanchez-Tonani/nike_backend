1ff85f01028f93849d50c18e3c12e2cc80d862a5
joaquin-sanchez-tonani <joaquinsancheztonani15@gmail.com>
2024-12-22 14:38:16 -0300

    app.py

--- app.py
+++ app.py
@@ -1,48 +1,40 @@
 from flask import Flask, request, jsonify
 from database import get_db_connection
 from flask_cors import CORS
+
 app = Flask(__name__)
-CORS(app, origins=["https://nike-joaquin.netlify.app/"])
+CORS(app, resources={r"/*": {"origins": "*"}})  # Ajusta los orígenes según tu necesidad
+
+def execute_query(query):
+    try:
+        db = get_db_connection()
+        cursor = db.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        cursor.close()
+        db.close()
+        return result
+    except Exception as e:
+        print("Database error:", e)
+        raise
+
 @app.route('/categories', methods=['POST'])
 def categories():
     try:
-        # Connection to the database
-        db = get_db_connection()
-        cursor = db.cursor()
-
-        # QUERY
         query = 'SELECT * FROM categories'
-        cursor.execute(query)
-        result = cursor.fetchall()
-
-        # Close database
-        db.close()
-        cursor.close()
-
+        result = execute_query(query)
         return jsonify(result)
     except Exception as e:
-        print("Error:", e)
-        return jsonify({"error": "Something went wrong"}), 500
+        return jsonify({"error": "Something went wrong", "details": str(e)}), 500
 
 @app.route('/products', methods=['POST'])
 def products():
     try:
-        # Connection to the database
-        db = get_db_connection()
-        cursor = db.cursor()
-
-        # QUERY
         query = 'SELECT * FROM products'
-        cursor.execute(query)
-        result = cursor.fetchall()
-
-        # Close database
-        db.close()
-        cursor.close()
-
+        result = execute_query(query)
         return jsonify(result)
     except Exception as e:
-        return jsonify("Error:", e)
+        return jsonify({"error": "Something went wrong", "details": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
